e7c76b926a25aacde55566b4c6e3be7dc5c40f8e
Marco Sciacovelli <marco.sciacovelli@gmail.com>
2021-11-30 10:13:28 +0100

    Add Chip Component

---
+++ src/components/Chip.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="chip" :class="bg_color">
+        <v-icon class="chip-icon" :color="color">{{ icon }}</v-icon>
+        <span class="chip-text">{{ $t(text) }}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            icon: {
+                type: String
+            },
+            color: {
+                type: String
+            },
+            text: {
+                type: String
+            },
+            bg_color: {
+                type: String
+            } 
+        }
+    }
+</script>
+
+<style>
+    .chip {
+        display: inline-block;
+        padding: 4px 8px;
+        border-radius: 8px;
+    }
+
+    .chip-icon {
+        padding-right: 4px;
+    }
+
+    .chip-text {
+        color: var(--v-black-base);
+        font-size: 12px;
+    }
+</style>
--- src/components/PrimaryButton.vue
+++ src/components/PrimaryButton.vue
@@ -18,24 +18,24 @@ export default {
 </script>
 
 <style>
-.btn {
-    background: radial-gradient(
-        105.58% 105.58% at 50% -5.58%,
-        var(--v-tertiary-base) 0%,
-        var(--v-secondary-base) 0.01%,
-        var(--v-primary-base) 100%
-    );
-    border-radius: 8px;
-    padding: 10px 40px;
-    transition: 0.5s;
-}
+    .btn {
+        background: radial-gradient(
+            105.58% 105.58% at 50% -5.58%,
+            var(--v-tertiary-base) 0%,
+            var(--v-secondary-base) 0.01%,
+            var(--v-primary-base) 100%
+        );
+        border-radius: 8px;
+        padding: 10px 40px;
+        transition: 0.5s;
+    }
 
-.btn:hover {
-    box-shadow: 0px 0px 10px rgba(56, 73, 86, 0.3);
-}
+    .btn:hover {
+        box-shadow: 0px 0px 10px rgba(56, 73, 86, 0.3);
+    }
 
-.btn-text {
-    color: var(--v-text_primary-base);
-    font-weight: 700;
-}
+    .btn-text {
+        color: var(--v-white-base);
+        font-weight: 700;
+    }
 </style>
